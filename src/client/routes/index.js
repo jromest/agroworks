07662911294ea5bf09af5a9a8ecc58a7e4ac65fa
jromest <jromestiller@gmail.com>
2019-02-18 21:09:29 +0800

    feat: use hashrouter for static serving

--- src/client/routes/index.js
+++ src/client/routes/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  BrowserRouter as Router, Switch, Route, withRouter
+  HashRouter as Router, Switch, Route, withRouter
 } from 'react-router-dom';
 
 import LandingPage from '../pages/LandingPage';
