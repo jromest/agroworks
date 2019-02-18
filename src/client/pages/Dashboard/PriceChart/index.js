b4018529a13ee366f0ef0fcbdd78b75204aa1693
jromest <jromestiller@gmail.com>
2019-02-18 20:31:38 +0800

    feat: add static data for price chart for presentation purposes

--- src/client/pages/Dashboard/PriceChart/index.js
+++ src/client/pages/Dashboard/PriceChart/index.js
@@ -1,36 +1,64 @@
 import React, { Component } from 'react';
 import Chart from './Chart';
 
+import rice from './data/rice.json';
+import wheat from './data/wheat.json';
+import maize from './data/maize.json';
+
 class PriceChart extends Component {
   state = {
     prices: null
   };
 
   componentDidMount() {
-    this.fetchPrices('rice');
+    this.fetchPricesLocal('rice');
   }
 
   changeCommodity = (e) => {
     e.preventDefault();
 
-    this.fetchPrices(e.target.value);
+    this.fetchPricesLocal(e.target.value);
   };
 
-  fetchPrices = (commodity) => {
-    fetch(`http://localhost:8080/api/prices/${commodity}`).then((res) => {
-      res.json().then((rawPrices) => {
-        const prices = rawPrices.map((rawPrice) => {
-          const price = Object.assign(rawPrice, {
-            date: new Date(rawPrice.date)
-          });
+  fetchPricesLocal = (commodity) => {
+    let commodityData;
 
-          return price;
-        });
+    if (commodity === 'rice') {
+      commodityData = rice;
+    } else if (commodity === 'wheat') {
+      commodityData = wheat;
+    } else if (commodity === 'maize') {
+      commodityData = maize;
+    } else {
+      return;
+    }
 
-        this.setState({ prices });
+    const prices = commodityData.map((rawPrice) => {
+      const price = Object.assign(rawPrice, {
+        date: new Date(rawPrice.date)
       });
+
+      return price;
     });
-  }
+
+    this.setState({ prices });
+  };
+
+  // fetchPrices = (commodity) => {
+  //   fetch(`http://localhost:8080/api/prices/${commodity}`).then((res) => {
+  //     res.json().then((rawPrices) => {
+  //       const prices = rawPrices.map((rawPrice) => {
+  //         const price = Object.assign(rawPrice, {
+  //           date: new Date(rawPrice.date)
+  //         });
+
+  //         return price;
+  //       });
+
+  //       this.setState({ prices });
+  //     });
+  //   });
+  // };
 
   renderDropDown = () => (
     <select onChange={this.changeCommodity}>
@@ -44,11 +72,7 @@ class PriceChart extends Component {
     const { prices } = this.state;
 
     if (!prices) {
-      return (
-        <div className="price-chart">
-          Loading...
-        </div>
-      );
+      return <div className="price-chart">Loading...</div>;
     }
 
     return (
